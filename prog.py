c456438d290e164f0b6a59c9679299641e1768ca
Daniel Dupree <57053860+danieldupree@users.noreply.github.com>
2020-01-13 14:25:01 -0800

    optional argument, not flag

--- prog.py
+++ prog.py
@@ -3,8 +3,7 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--verbose", help="increase output verbosity",
-                    action="store_true")
+parser.add_argument("--verbosity", help="increase output verbosity")
 args = parser.parse_args()
 if args.verbosity:
     print("verbosity turned on")
